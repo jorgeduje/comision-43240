f620a3c4838c674f2e7ea028a677333d94243375
jorgeduje <jorgeduje4@gmail.com>
2023-05-18 12:00:59 -0300

    se trabaja con componentes y props

--- src/App.jsx
+++ src/App.jsx
@@ -1,17 +1,26 @@
+import { useState } from "react";
+import Navbar from "./components/layout/navbar/Navbar";
+import { ProductsList } from "./components/pages/productsList/ProductsList";
 
-function App() {
-  const saludar = () => {
-    console.log("Hola como estas? bienvendio");
-  };
+const App = () => {
+  const [nombre, setNombre] = useState("pepe");
+  const [isDark, setIsDark] = useState(false);
 
+  console.log(isDark);
+
+  let edad = 22;
   return (
-    <button
-      onClick={saludar}
-      style={{ backgroundColor: "blue", padding: "10px 20px" }}
-    >
-      saludar
-    </button>
+    <div>
+      <Navbar />
+      <ProductsList nombre={nombre} edad={edad} />
+      <button onClick={() => setNombre("maria")}>Cambiar nombre</button>
+      <button onClick={() => setIsDark(!isDark)}>
+       cambiar a {isDark ? "cambiar a claro" : "cambiar a oscuro"}
+      </button>
+    </div>
   );
-}
+};
 
 export default App;
+// ProductsList(nombre)
+
