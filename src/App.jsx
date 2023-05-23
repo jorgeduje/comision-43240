77aa88aa09439946b317e2fe28fdebc40f006985
jorgeduje <jorgeduje4@gmail.com>
2023-05-23 11:48:59 -0300

    .

--- src/App.jsx
+++ src/App.jsx
@@ -1,23 +1,14 @@
-import { useState } from "react";
 import Navbar from "./components/layout/navbar/Navbar";
 import ProductsListContainer from "./components/pages/productsList/ProductsListContainer";
 
 const App = () => {
-  const [montado, setMontado] = useState(false);
-  const [nombre, setNombre] = useState("pepito");
-
   return (
     <div>
       <Navbar />
 
-      {montado && <ProductsListContainer nombre={nombre} />}
-
-      <button onClick={() => setMontado(!montado)}>Montar componente</button>
-
-      <button onClick={() => setNombre("maria")}>cambiar nombre</button>
+      <ProductsListContainer />
     </div>
   );
 };
 
 export default App;
-// ProductsList(nombre)
