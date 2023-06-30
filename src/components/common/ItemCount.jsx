147886e6eb0c8dc47233beb7e28540cac35a52aa
jorgeduje <jorgeduje4@gmail.com>
2023-06-30 13:02:56 -0300

    se trabajo con autenticacion de firebase

--- src/components/common/ItemCount.jsx
+++ src/components/common/ItemCount.jsx
@@ -1,23 +1,48 @@
 import { Button } from "@mui/material";
 import { useCount } from "../hooks/useCount";
+import { useState } from "react";
+import { useNavigate } from "react-router-dom";
 
 export const ItemCount = ({ stock, initial = 1, onAdd }) => {
   const { count, decrement, increment } = useCount(initial, stock);
+  const [isAdd, setIsAdd] = useState(false);
+  const navigate = useNavigate();
 
   return (
     <>
       <div style={{ marginBottom: "20px", display: "flex", gap: "30px" }}>
-        <Button variant="contained" onClick={decrement}>
-          -
-        </Button>
-        <h2>{count}</h2>
-        <Button variant="contained" onClick={increment}>
-          +
-        </Button>
+        {!isAdd && (
+          <>
+            <Button variant="contained" onClick={decrement}>
+              -
+            </Button>
+            <h2>{count}</h2>
+            <Button variant="contained" onClick={increment}>
+              +
+            </Button>
+          </>
+        )}
 
-        <Button variant="contained" onClick={() => onAdd(count)}>
-          Agregar al carrito
-        </Button>
+        {isAdd ? (
+          <Button
+            variant="contained"
+            onClick={() => {
+              navigate("/carrito");
+            }}
+          >
+            Ir al Carrito
+          </Button>
+        ) : (
+          <Button
+            variant="contained"
+            onClick={() => {
+              onAdd(count);
+              setIsAdd(true);
+            }}
+          >
+            Agregar al carrito
+          </Button>
+        )}
       </div>
     </>
   );
--- src/firebaseConfig.js
+++ src/firebaseConfig.js
@@ -1,5 +1,12 @@
 import { initializeApp } from "firebase/app";
-import { getFirestore } from "firebase/firestore"
+import { getFirestore } from "firebase/firestore";
+import {
+  getAuth,
+  signInWithEmailAndPassword,
+  createUserWithEmailAndPassword,
+  signInWithPopup,
+  GoogleAuthProvider,
+} from "firebase/auth";
 
 const firebaseConfig = {
   apiKey: "AIzaSyCRcWayegvwBj6jiwt3lEHT1bWZXiNmPCQ",
@@ -7,8 +14,36 @@ const firebaseConfig = {
   projectId: "comision-43240",
   storageBucket: "comision-43240.appspot.com",
   messagingSenderId: "233778535343",
-  appId: "1:233778535343:web:b597f30f07145f7d132d5a"
+  appId: "1:233778535343:web:b597f30f07145f7d132d5a",
 };
 
 const app = initializeApp(firebaseConfig);
-export const db = getFirestore(app)
+export const db = getFirestore(app);
+
+const auth = getAuth(app);
+
+export const login = async ({ email, password }) => {
+  try {
+    return await signInWithEmailAndPassword(auth, email, password);
+  } catch (error) {
+    console.error(error);
+  }
+};
+
+export const register = async ({ email, password }) => {
+  try {
+    return await createUserWithEmailAndPassword(auth, email, password);
+  } catch (error) {
+    console.error(error);
+  }
+};
+
+const googleProvider = new GoogleAuthProvider();
+
+export const loginWithGoogle = async () => {
+  try {
+    return await signInWithPopup(auth, googleProvider);
+  } catch (error) {
+    console.error(error);
+  }
+};
